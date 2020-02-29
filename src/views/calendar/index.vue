28665e356e42855c30c3691f554e5684dce60992
lovecode20192019utl <guoyuanwei2018@outlook.com>
2020-02-29 14:56:16 +0800

    删掉了node_modules这个大文件夹，请自信通过命令npm安装

---
+++ src/views/calendar/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-container">
+    
+<el-calendar v-model="value">
+</el-calendar>
+ 
+ </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        value: new Date()
+      }
+    }
+  }
+</script>
